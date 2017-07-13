24950c9115dc569fcfb2fe02342ce2c0e1819413
Threefold Burly <threefold.burly@gmail.com>
2017-07-13 03:34:34 +0200

    Dodanie modulu wspolne.py

---
+++ wspolne.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Moduł współdzielony, z którego pozostałe importują stałe i funkcje.
+"""
+
+ZNACZNIK_PUSTY = "0"
+ZNACZNIK_PUDLO = "x"
+ZNACZNIK_TRAFIONY = "T"
+ZNACZNIK_ZATOPIONY = "Z"
+ZNACZNIK_STATEK = "&"
+ZNACZNIK_OBWIEDNIA = "."
+KIERUNKI = ["prawo", "lewo", "gora", "dol"]
+
