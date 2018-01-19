0b85c288ebbbfec5509a96d8a3e361299c9db204
tburly <threefold.burly@gmail.com>
2018-01-19 21:02:28 +0100

    Klasa PlanszaGUI, metoda buduj_sie(): etykiety rzędów i kolumn

--- gui.py
+++ gui.py
@@ -22,9 +22,15 @@ class PlanszaGUI:
         self.zawartosc.grid(column=0, row=0)
 
     def buduj_sie(self):
+        # etykiety kolumn i rzędów
+        for kolumna in range(self.plansza.kolumny):
+            ttk.Label(self.zawartosc, text=str(kolumna + 1), anchor=CENTER).grid(row=0, column=kolumna + 1, sticky=W + E, pady=(0, 3))
+        for rzad in range(self.plansza.rzedy):
+            ttk.Label(self.zawartosc, text=str(rzad + 1)).grid(column=0, row=rzad + 1, sticky=E, padx=(0, 6))
+        # pola
         for i in range(self.plansza.kolumny):
             for j in range(self.plansza.rzedy):
-                self.matryca_pol[j][i] = ttk.Button(self.zawartosc, text="S", width=2).grid(column=i, row=j, sticky=W, pady=2, padx=2)
+                self.matryca_pol[j][i] = ttk.Button(self.zawartosc, text="S", width=2).grid(column=i + 1, row=j + 1, sticky=W, pady=2, padx=2)
 
 
 def main():
@@ -32,13 +38,14 @@ def main():
     Uruchamia skrypt
     """
     # przygotowanie planszy
-    plansza = Plansza(20, 20)
+    plansza = Plansza(15, 20)
     plansza.drukuj_sie()
     plansza.wypelnij_statkami()
     plansza.drukuj_sie()
 
     # GUI
     root = Tk()
+    root.title("Statki")
     gui = PlanszaGUI(root, plansza)
     gui.buduj_sie()
     root.resizable(False, False)
