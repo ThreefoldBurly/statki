51fd93e3da7ab556063bdaf1152bfc3cd868e486
tburly <threefold.burly@gmail.com>
2018-01-20 00:34:15 +0100

    Klasa PlanszaGUI, metoda sprawdz_pole() dodana jako callback każdego z przycisków (wstępna implementacja)

--- gui.py
+++ gui.py
@@ -18,19 +18,50 @@ class PlanszaGUI:
         self.plansza = plansza
         self.matryca_pol = [[0 for kolumna in range(self.plansza.kolumny)] for rzad in range(self.plansza.rzedy)]
         # GUI
-        self.zawartosc = ttk.Frame(root, padding=10)
-        self.zawartosc.grid(column=0, row=0)
+        self.ramka_pol = ttk.Frame(root, padding=10)
+        self.ramka_pol.grid(column=0, row=0)
 
     def buduj_sie(self):
         # etykiety kolumn i rzędów
         for kolumna in range(self.plansza.kolumny):
-            ttk.Label(self.zawartosc, text=str(kolumna + 1), anchor=CENTER).grid(row=0, column=kolumna + 1, sticky=W + E, pady=(0, 3))
+            ttk.Label(
+                self.ramka_pol,
+                text=str(kolumna + 1),
+                anchor=CENTER
+            ).grid(
+                row=0,
+                column=kolumna + 1,
+                sticky=W + E,
+                pady=(0, 3)
+            )
         for rzad in range(self.plansza.rzedy):
-            ttk.Label(self.zawartosc, text=str(rzad + 1)).grid(column=0, row=rzad + 1, sticky=E, padx=(0, 6))
+            ttk.Label(
+                self.ramka_pol,
+                text=str(rzad + 1)
+            ).grid(
+                column=0,
+                row=rzad + 1,
+                sticky=E,
+                padx=(0, 6)
+            )
         # pola
         for i in range(self.plansza.kolumny):
             for j in range(self.plansza.rzedy):
-                self.matryca_pol[j][i] = ttk.Button(self.zawartosc, text="S", width=2).grid(column=i + 1, row=j + 1, sticky=W, pady=2, padx=2)
+                self.matryca_pol[j][i] = ttk.Button(
+                    self.ramka_pol,
+                    text="",
+                    width=2,
+                    command=lambda x=i + 1, y=j + 1: self.sprawdz_pole(x, y)  # lambda bez własnych argumentów (w formie: lambda: self.sprawdz_pole(i+1, j+1) nie zadziała prawidłowo w tym przypadku - zostaną przypisane wartości zmiennych "i" i "j" z ostatniej iteracji do wszystkich przycisków, więcej tutaj: https://stackoverflow.com/questions/2295290/what-do-lambda-function-closures-capture/23557126)
+                ).grid(
+                    column=i + 1,
+                    row=j + 1,
+                    sticky=W,
+                    pady=2,
+                    padx=2
+                )
+
+    def sprawdz_pole(self, kolumna, rzad):
+        print("Kliknięcie w polu: ({},{})".format(kolumna, rzad))
 
 
 def main():
